41da7d120634f30fcd6045462cf5126abd10c41e
Wajiha <ayenaansari@gmail.com>
2024-09-02 18:07:58 +0500

    inital commit

---
+++ popup.js
@@ -0,0 +1,44 @@
+const btn = document.querySelector('.changeColorBtn');
+const colorGrid = document.querySelector('.colorGrid');
+const colorValue = document.querySelector('.colorValue');
+
+btn.addEventListener('click', async () => {
+    chrome.storage.sync.get('color', ({ color }) => {
+        console.log('color: ', color);
+    });
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript(
+        {
+            target: { tabId: tab.id },
+            function: pickColor,
+        },
+        async (injectionResults) => {
+            const [data] = injectionResults;
+            if (data.result) {
+                const color = data.result.sRGBHex;
+                colorGrid.style.backgroundColor = color;
+                colorValue.innerText = color;
+                try {
+                    await navigator.clipboard.writeText(color);
+                } catch (err) {
+                    console.error(err);
+                }
+            }
+        }
+    );
+});
+
+//This is running inside the chrome page we are working our extension Inside . . . 
+async function pickColor(){
+    try{
+        //picker
+        const eyeDropper  = new EyeDropper();
+        return await eyeDropper.open();
+        console.log(selectedColor);
+        
+    }
+    catch(err){
+        console.error(err);
+    }
+}
